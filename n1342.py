9e0e274ed371652cba20e442e2926779374cba1d
anarkaytis <anarkaytis@gmail.com>
2026-01-25 17:33:48 -0800

    Create n1342.py

---
+++ n1342.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        cnt = 0
+        while num > 0:
+            num = num // 2 if num % 2 == 0 else num - 1
+            cnt += 1
+        return cnt
