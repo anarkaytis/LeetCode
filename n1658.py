f663f431cda7b7ae279c6f67ab3d6c5612b1c9b0
anarkaytis <anarkaytis@gmail.com>
2026-04-27 20:11:12 -0700

    Create n1658.py

---
+++ n1658.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        n = len(nums)
+        ps = {}
+        ps[0] = 0
+        prefix = 0
+        for i in range(n):
+            prefix += nums[i]
+            ps[prefix] = i + 1
+        suffix = 0
+        result = n + 1
+        if x in ps:
+            result = ps[x]
+        for i in range(n - 1, -1, -1):
+            suffix += nums[i]
+            diff = x - suffix
+            if diff in ps:
+                result = min(result, ps[diff] + n - i)
+        return result if result != n + 1 else -1
