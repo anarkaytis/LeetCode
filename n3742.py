cf9aa43fa96114479c239e76f12767c52014c2d4
anarkaytis <anarkaytis@gmail.com>
2026-04-30 19:25:50 -0700

    Update n3742.py

--- n3742.py
+++ n3742.py
@@ -2,20 +2,24 @@ class Solution:
     def maxPathScore(self, grid: List[List[int]], k: int) -> int:
         m = len(grid)
         n = len(grid[0])
-        dp = {}
-        dp[(0,0)] = {k : 0}
+        dp = [{} for _ in range(n)]
+        dp[0] = {k : 0}
 
         for x in range(m):
-            for y in range(n):
-                if (x == 0) and (y == 0):
-                    continue
-                top = dp.get((x - 1, y), {})
-                left = dp.get((x, y - 1), {})
-                curr = {}
+            if grid[x][0] != 0:
+                updated = {}
+                for key, value in dp[0].items():
+                    if key > 0:
+                        updated[key - 1] = value + grid[x][0]
+                dp[0] = updated
+            for y in range(1, n):
+                top = dp[y]
+                left = dp[y - 1]
+                updated = {}
                 for key in (top.keys() | left.keys()):
                     if grid[x][y] == 0:
-                        curr[key] = max(top.get(key, 0), left.get(key, 0))
-                    elif key >= 1:
-                        curr[key - 1] = max(top.get(key, 0), left.get(key, 0)) + grid[x][y]
-                dp[(x, y)] = curr
-        return max(dp[(m - 1, n - 1)].values()) if dp[(m - 1, n - 1)].values() else -1
+                        updated[key] = max(top.get(key, 0), left.get(key, 0))
+                    elif key > 0:
+                        updated[key - 1] = max(top.get(key, 0), left.get(key, 0)) + grid[x][y]
+                dp[y] = updated
+        return max(dp[n - 1].values()) if dp[n - 1].values() else -1
